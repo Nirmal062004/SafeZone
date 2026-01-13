ea0d333a5f461bc62f175d3a1e4dc30b3bceacc1
Nirmal062004 <nirmal.arsade123@gmail.com>
2026-01-14 02:23:23 +0530

    code updated

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,32 +1,23 @@
-// Import the functions you need from the SDKs you need
-<<<<<<< HEAD
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';  // Firebase Authentication
-import { getFirestore } from 'firebase/firestore';  // Firestore Database
-=======
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
->>>>>>> 47cda08b972f56a2edb922e140c532d54c9a89b2
+// firebase.js
 
-// Your web app's Firebase configuration
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID" // Optional
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-<<<<<<< HEAD
 
-// Initialize Firebase Authentication & Firestore
+// Firebase services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 
-=======
-const analytics = getAnalytics(app);
->>>>>>> 47cda08b972f56a2edb922e140c532d54c9a89b2
+export default app;
